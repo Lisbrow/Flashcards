77a9eac08eaf61be63dce2f4b2f643c17d87dab3
Lisbrow <l.brown91910@gmail.com>
2024-10-21 18:01:02 -0700

    Add new quiz functionality to Quizzes

---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    quizzes: {}
+};
+
+const quizzesSlice = createSlice({
+    name: 'quizzes',
+    initialState: initialState,
+    reducers: {
+        addQuiz: (state, action) => {
+            const { id, name, topicId } = action.payload;
+            state.quizzes[id] = {
+                id,
+                name,
+                topicId,
+                cardIds: []
+            };
+        },
+    },
+});
+
+export const { addQuiz } = quizzesSlice.actions;
+export const selectQuizzes = (state) => state.quizzes.quizzes;
+
+export default quizzesSlice.reducer;
